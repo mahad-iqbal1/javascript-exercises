e1294f4aa798f3a4767f2e5ff9844025f386bbd6
mahad-iqbal1 <135532774+mahad-iqbal1@users.noreply.github.com>
2026-01-28 17:41:02 +0500

    Completed JavaScript exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, index) => {
+    acc = acc + index;
+    return acc;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  for (let i = 0; i < arr.length; i++) {
+    product = product * arr[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact = fact * i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
