a7fba599a090e754c45a89c6236e1ee2e3ef1e7e
mahad-iqbal1 <135532774+mahad-iqbal1@users.noreply.github.com>
2026-01-30 22:37:14 +0500

    Completed exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(arg) {
+  let num;
+  if (typeof arg !== 'number') {
+    num = parseInt(arg)
+  } else {
+    num = arg;
+  }   
 
+  let first = 1;
+  let second = 1;
+  let fib = 0;
+
+  if (num === 1 || num === 2) {
+    return 1;
+  }
+
+  if (num < 0) {
+    return "OOPS";
+  }
+
+  for (let i = 0; i <= num - 3; i++) {
+    fib = first + second;
+    second = first;
+    first = fib;
+  }
+
+  return fib;
 };
 
 // Do not edit below this line
