233124f732d697b91d7b1e1ad728340d8cb2fa3a
mahad-iqbal1 <135532774+mahad-iqbal1@users.noreply.github.com>
2026-01-31 23:00:14 +0500

    Completed exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(objArr) {
+  // First calculate the ages of each person object in the array
+  let modifiedArr = objArr.map(person => ({
+    name: person.name,
+    age: person.yearOfDeath != null
+      ? person.yearOfDeath - person.yearOfBirth
+      : new Date().getFullYear() - person.yearOfBirth,
+  }))
+  // Find the oldest age
+  const oldestPerson = modifiedArr.reduce((oldest, current) => {
+    return current.age > oldest.age ? current : oldest;
+  });
 
+  return oldestPerson;
+
+
+  /*Personal feedback for this solution:
+    1) Don't throw away data (keep the original data) 
+    2) Try and conserve memory (no unnecessary looping) 
+    3) Use additional functions for different tasks. 
+       One function should perform only one thing. 
+       In your solution you calculate the ages and then find the oldest both in the same function.
+       In the official solution, they create a separate function for getting the age. 
+       That's much more scalable and reusable. 
+    4) Do not give up. Keep going at it relentlessly.
+  */
 };
 
 // Do not edit below this line
